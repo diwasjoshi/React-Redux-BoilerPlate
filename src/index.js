d6bdbeb87738f86de51445389b0836f634db270f
Diwas Joshi <dj.dij123@gmail.com>
2017-10-24 17:01:08 +0530

    react setup

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-console.log('starting');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <div>Minimal React Redux Setup</div>,
+  document.getElementById('app')
+);
 
 module.hot.accept();
